b7903819697e38e2126911838431e4d9c6bb727e
Matias-Antilef <matias.romero.dv@gmail.com>
2025-02-14 13:41:08 -0300

    auth service, login
    crear la clase login y manejar el accessToken para autenticación

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private LOGIN_URL = 'https://dummyjson.com/auth/login';
+  private tokenKey = 'authToken';
+
+  constructor(private httpClient: HttpClient, private router: Router) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.httpClient
+      .post<any>(this.LOGIN_URL, { username, password })
+      .pipe(
+        tap((response) => {
+          if (response.accessToken) {
+            console.log(response.accessToken);
+          }
+        })
+      );
+  }
+
+  private setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  private getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  isAuthenticated(): boolean {
+    const token = this.getToken();
+
+    if (!token) {
+      return false;
+    }
+
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const exp = payload.exp * 1000;
+    return Date.now() < exp;
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.router.navigate(['/login']);
+  }
+}
