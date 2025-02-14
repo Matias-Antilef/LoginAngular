b87fea09c6cf284df9aabb1da372946481eb064f
Matias-Antilef <matias.romero.dv@gmail.com>
2025-02-14 14:35:16 -0300

    auth guard
    Integración de ruta protegida y redirección.
    
    Si esta autenticado, no puede ir al login.

--- src/app/core/guards/authenticated.guard.ts
+++ src/app/core/guards/authenticated.guard.ts
@@ -2,13 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const AuthGuard: CanActivateFn = (route, state) => {
+export const AuthenticatedGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.isAuthenticated()) {
-    return true;
+    return router.navigate(['/protected-page']);
   } else {
-    return router.navigate(['/login']);
+    return true;
   }
 };
